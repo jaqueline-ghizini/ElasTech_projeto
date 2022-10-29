557ffd4bb90ffa422863ad47114cf190bba6eb1e
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2022-10-29 11:20:41 -0300

    Arquivo inicial spring boot]

---
+++ soulBank/soulBank/src/main/java/codingDreams/SoulBankApplication.java
@@ -0,0 +1,13 @@
+package codingDreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulBankApplication.class, args);
+	}
+
+}
